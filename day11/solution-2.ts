281ecfa2eb3df9708686a2f0b4469c6fe8267149
Kent Hu <kenthu@kenthu.net>
2022-12-11 11:19:34 -0800

    Add explanation for day 11 part 2

--- day11/solution-2.ts
+++ day11/solution-2.ts
@@ -96,7 +96,7 @@ const ingestInput = (filename: string): Monkey[] => {
 
 const result = (filename: string): number => {
   const monkeys = ingestInput(filename);
-  const leastCommonMultiple = monkeys.reduce((acc, monkey) => acc * monkey.divisor, 1);
+  const allDivisors = monkeys.reduce((acc, monkey) => acc * monkey.divisor, 1);
 
   _.times(ROUNDS, () => {
     monkeys.forEach((monkey) => {
@@ -106,7 +106,31 @@ const result = (filename: string): number => {
         // Test and move
         const nextMonkey =
           newWorryLevel % monkey.divisor === 0 ? monkey.nextMonkeyIfTrue : monkey.nextMonkeyIfFalse;
-        monkeys[nextMonkey].items.push(newWorryLevel % leastCommonMultiple);
+
+        /**
+         * How can we keep worry levels manageable? Well, if the test is whether the worry level is
+         * divisible by 23, then we can modulo by 23, which would not change the test result.
+         *
+         * But what about the other operations performed before the next time we check if the worry
+         * level is divisible by 23?
+         * - Addition: This is fine. (n % 23 + x) % 23 = (n + x) % 23
+         * - Multiplication: This is also fine. ((n % 23) * x) % 23 = (n * x) % 23. Why? If n % 23
+         *   == n, then it's obviously the same. But even if n % 23 != n, that just means n % 23 = n
+         *   - 23, so that's ((n - 23) * x) % 23. Since we're just shifting the ((n - 23) * x)
+         *     expression by some multiple of 23, that doesn't change the ultimate modulo result.
+         * - Squaring: This is the same as multiplication
+         *
+         * The above properties also apply when executing module on some multiple of our original
+         * divisor. For example:
+         *
+         * (n % 46 + x) % 23 = (n + x) % 23
+         *
+         * So that means we can take the product of all our divisors (23 * 19 * 13 * 17) and be
+         * confident that if we modulo this product, that won't change any of the test results
+         * (e.g., divisible by 23 or divisible by 19), even if there are addition/multiplication
+         * operations performed in between.
+         */
+        monkeys[nextMonkey].items.push(newWorryLevel % allDivisors);
 
         monkey.inspections++;
       }
